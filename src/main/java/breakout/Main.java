13090f46bbd3e015638f8e4983f8bb0a099beb72
Justin Aronwald <jga18@duke.edu>
2025-01-15 15:31:58 -0500

    Feature BREAK-01 Ball starts in the middle and goes up and down. Logic is handled for bouncing off walls.

--- src/main/java/breakout/Main.java
+++ src/main/java/breakout/Main.java
@@ -34,64 +34,50 @@ public class Main extends Application {
     public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
     public static final Paint BACKGROUND = Color.AZURE;
 
-    public class Ball {
+    public static final int BALL_SIZE = 20;
+    public static final int BALL_SPEED = 100;
 
-        private String img;
+    private Ball ball;
+
+    public class Ball {
         private int size;
         private int speed;
-        private int direction;
         private int screen_width;
         private int screen_height;
-        private ImageView myBall;
+        private Circle myBall;
         private int xDirection;
         private int yDirection;
-        private double startX;
-        private double startY;
 
-        public Ball(int size, int speed, int direction, int screen_width, int screen_height,
+        public Ball(int size, int speed, int screen_width, int screen_height,
             int xDirection, int yDirection) {
             this.size = size;
             this.speed = speed;
-            this.direction = direction;
-            this.img = "duke-seal-logo.png";
             this.screen_width = screen_width;
             this.screen_height = screen_height;
             this.xDirection = xDirection;
             this.yDirection = yDirection;
 
-            // setup the imageView
-            myBall = new ImageView(new Image(Main.class.getResourceAsStream(img)));
-            myBall.setFitWidth(size);
-            myBall.setFitHeight(size);
-
-            Random rand = new Random();
-            double randomX = rand.nextInt(screen_width - (int) myBall.getFitWidth());
-            myBall.setX(randomX);
-
-            double randomY = rand.nextInt(screen_height - (int) myBall.getFitHeight());
-            myBall.setY(randomY);
+            this.myBall = new Circle(size/2);
+            this.myBall.setFill(Color.WHITE);
+            this.myBall.setCenterX(screen_width/2);
+            this.myBall.setCenterY(screen_height/2);
         }
 
-        public ImageView getImageView() {
+        public Circle getBall() {
             return myBall;
         }
 
         public void moveBall(double elapsedTime) {
-            myBall.setX(myBall.getX() + xDirection * speed * elapsedTime);
-            myBall.setY(myBall.getY() + yDirection * speed * elapsedTime);
+            myBall.setCenterX(myBall.getCenterX() + xDirection * speed * elapsedTime);
+            myBall.setCenterY(myBall.getCenterY() + yDirection * speed * elapsedTime);
 
-            if (myBall.getX() + size >= SIZE) {
-                xDirection *= -1;
-            } else if (myBall.getX() <= 0) {
+            if (myBall.getCenterX() - myBall.getRadius() <= 0 || myBall.getCenterX() + myBall.getRadius() >= SIZE) {
                 xDirection *= -1;
             }
 
-            if (myBall.getY() + size >= SIZE) {
-                yDirection *= -1;
-            } else if (myBall.getY() <= 0) {
+            if (myBall.getCenterY() - myBall.getRadius() <= 0 || myBall.getCenterY() + myBall.getRadius() >= SIZE) {
                 yDirection *= -1;
             }
-
         }
     }
 
@@ -101,20 +87,34 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage stage) {
-        Circle shape = new Circle(200, 200, 40);
-        shape.setFill(Color.LIGHTSTEELBLUE);
+
+        ball = new Ball(BALL_SIZE, BALL_SPEED, SIZE, SIZE, 0, 1);
 
         Group root = new Group();
-        root.getChildren().add(shape);
+        root.getChildren().add(ball.getBall());
 
         Scene scene = new Scene(root, SIZE, SIZE, DUKE_BLUE);
         stage.setScene(scene);
 
         stage.setTitle(TITLE);
         stage.show();
+
+        // set the animation to let the game run
+        Timeline animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(new KeyFrame(Duration.seconds(SECOND_DELAY), e -> step(SECOND_DELAY)));
+        animation.play();
     }
 
-        public static void main (String[]args){
+    // the following code has been adapted from the bounce lab
+    // Handle game "rules" for every "moment":
+    private void step (double elapsedTime) {
+        // update "actors" attributes a little bit at a time and at a "constant" rate (no matter how many frames per second)
+        ball.moveBall(elapsedTime);
+    }
+
+
+    public static void main (String[]args){
             launch(args);
         }
 
