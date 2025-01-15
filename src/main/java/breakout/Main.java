fc23a89fec7ecda99af67ecb33b96ad182b79096
Justin Aronwald <jga18@duke.edu>
2025-01-15 18:26:31 -0500

    Refactor BREAK-02: Small paddle UI fixed + main content

--- src/main/java/breakout/Main.java
+++ src/main/java/breakout/Main.java
@@ -36,10 +36,10 @@ public class Main extends Application {
     public static final int BALL_SPEED = 100;
 
     public static final int PADDLE_WIDTH = 100;
-    public static final int PADDLE_HEIGHT = 20;
+    public static final int PADDLE_HEIGHT = 10;
     public static final int PADDLE_SPEED = 20;
     public static final int PADDLE_X_POS = 100;
-    public static final int PADDLE_Y_POS = 300;
+    public static final int PADDLE_Y_POS = 350;
 
 
     // This scene contains the various shapes and has methods that act upon them
