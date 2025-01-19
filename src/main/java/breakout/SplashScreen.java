7b6e34ff9c4e0dbcc9533588d5e3d894e6115462
Justin Aronwald <jga18@duke.edu>
2025-01-19 17:42:34 -0500

    Feature BREAK-10: Started the splashscreen class for intro

---
+++ src/main/java/breakout/SplashScreen.java
@@ -0,0 +1,40 @@
+package breakout;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class SplashScreen {
+  private static final int SIZE = 400;
+
+  public Scene createIntroScreen(Stage stage, String title, String description) {
+    Group root = new Group();
+
+    Text titleText = createText(title, SIZE / 2.0, SIZE/3.0, Color.WHITE, new Font(18));
+    Text descriptionText = createText(description, SIZE / 2.5, SIZE/3.5, Color.WHITE, new Font(18));
+
+    root.getChildren().addAll(titleText, descriptionText);
+
+    Scene introScene = new Scene(root, SIZE, SIZE, Color.BLACK);
+
+    return introScene;
+
+
+  }
+
+  //creates, sets, and places text
+  private Text createText(String content, double x, double y, Color color, Font font) {
+    Text text = new Text();
+    text.setText(content);
+    text.setX(x);
+    text.setY(y);
+    text.setFill(color);
+    text.setFont(font);
+    return text;
+  }
+
+
+}
